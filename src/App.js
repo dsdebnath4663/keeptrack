369c5132a0c9dd2b87897ddf74b75a6135685dfd
debnath <dsdebnath4663@gmail.com>
2024-03-31 19:40:17 +0530

    basic todo app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
+import {
+  Route,
+  Switch,
+  Link,
+  Routes,
+  NavLink,
+  useLocation,
+} from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
 import "./App.css";
 import ProjectsPage from "./projects/ProjectsPage";
+import HomePage from "./projects/home/HomePage";
+import ProjectPage from "./projects/ProjectPage";
 
 function App() {
+  let location = useLocation();
   return (
-    <div className="ExampleForm">
-      <h1>Projects</h1>
-      <ProjectsPage />
-    </div>
+    <>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="container">
+        <TransitionGroup>
+          <CSSTransition
+            key={location.key}
+            classNames="fade"
+            timeout={{ enter: 400, exit: 200 }}
+          >
+            <Routes location={location}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/projects" element={<ProjectsPage />} />
+              <Route path="/projects/:id" element={<ProjectPage />} />
+            </Routes>
+          </CSSTransition>
+        </TransitionGroup>
+      </div>
+    </>
   );
 }
 
